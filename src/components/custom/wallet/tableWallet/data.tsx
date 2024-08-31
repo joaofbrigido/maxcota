507bd020257e8d6ffb7ea268b00da2524d544899
João Fernando do Amaral Brigido <70164946+joaofbrigido@users.noreply.github.com>
2024-08-31 11:34:50 -0300

    ajuste tamanho tabela (minha carteira)

--- src/components/custom/wallet/tableWallet/data.tsx
+++ src/components/custom/wallet/tableWallet/data.tsx
@@ -145,7 +145,7 @@ export function TableWallet<TData, TValue>({
                   data-state={row.getIsSelected() && "selected"}
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
+                    <TableCell key={cell.id} className="py-2">
                       {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
